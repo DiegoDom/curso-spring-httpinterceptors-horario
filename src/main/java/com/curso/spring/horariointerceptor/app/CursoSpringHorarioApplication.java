92f080234d0ad0482d7235467c0ab978ca033197
DiegoDom <dominguezdiex@gmail.com>
2022-04-01 13:56:52 -0600

    Ejemplo de interceptors http, restriccion de carga por horario

---
+++ src/main/java/com/curso/spring/horariointerceptor/app/CursoSpringHorarioApplication.java
@@ -0,0 +1,13 @@
+package com.curso.spring.horariointerceptor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringHorarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringHorarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/spring/horariointerceptor/app/CursoSpringHorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.spring.horariointerceptor.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringHorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
